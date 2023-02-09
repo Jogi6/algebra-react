b5651931b514821bd82c5c5f284baa60027666bb
Josip <hyperv@ucione.local>
2023-02-09 19:36:37 +0100

    Start

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
+import { Component } from "react";
+import Button from "./Button";
+import Clock from "./Clock";
 
-const App = () => {
-  return (
-    <div></div>
-  );
+class App extends Component{
+
+  state = {
+    showClock: true
+  }
+
+  render () {
+const {showClock} = this.state;
+
+    return (
+      <div>
+        <Clock />
+        <Button>
+          { showClock ? "Ukloni me" : "Prikaži me"}
+        </Button>
+    </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Clock.jsx
@@ -0,0 +1,5 @@
+const Clock = () => {
+    return <div><h3>Ovdje je sat</h3></div>
+}
+
+export default Clock;
