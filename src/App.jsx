8ceef3059044f7391ab7fc270d576c63447a01ba
Josip <hyperv@ucione.local>
2023-02-09 21:09:26 +0100

    Added users object and changed App.jsx to class

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
+class App extends Component{
 
-const App = () => {
-  return (
-    <div></div>
-  );
+  state = {
+    users = [
+      {
+        ime: "Branko",
+        prezime: "Branković",
+        dob: 32,
+      },
+      {
+        ime: "Janko",
+        prezime: "Janković",
+        dob: 42,
+      },
+      {
+        ime: "Stanko",
+        prezime: "Stanković",
+        dob: 52,
+      },
+      
+    ]
+  }
+
+  render () {
+
+    return (
+      <div></div>
+    );
+  }
 }
 
 export default App;
